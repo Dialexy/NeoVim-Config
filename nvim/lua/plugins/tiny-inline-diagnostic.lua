46273a3123d5f90380c9b96d5713081d9a68c10d
Dialexy <filiperamos212@gmail.com>
2025-12-08 14:45:39 +0000

    updated conf

---
+++ nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"rachartier/tiny-inline-diagnostic.nvim",
+		event = "VeryLazy",
+		priority = 1000,
+		config = function()
+			require("tiny-inline-diagnostic").setup({
+				preset = "modern",
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			diagnostics = {
+				virtual_text = false,
+			},
+		},
+	},
+}
