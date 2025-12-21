79b005b2ac8b680828e298a15375a429fece4b30
Dialexy <filiperamos212@gmail.com>
2025-12-21 17:26:25 +0000

    Updated config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,20 +1,6 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
--- Do things without affecting the registers
-keymap.set("n", "x", '"_x')
-keymap.set("n", "<Leader>p", '"-2p')
-keymap.set("n", "<Leader>P", '"-2P')
-keymap.set("v", "<Leader>p", '"-2p')
-keymap.set("n", "<Leader>c", '"_c')
-keymap.set("n", "<Leader>C", '"_C')
-keymap.set("v", "<Leader>c", '"_c')
-keymap.set("v", "<Leader>C", '"_C')
-keymap.set("n", "<Leader>d", '"_d')
-keymap.set("n", "<Leader>D", '"_D')
-keymap.set("v", "<Leader>d", '"_d')
-keymap.set("v", "<Leader>D", '"_D')
-
 -- Delete a word backwards
 keymap.set("n", "dw", 'vb"_d')
 
