4239ae9a11ab64b3e7a7c34153fa5cc5d5b03a81
Dialexy <filiperamos212@gmail.com>
2026-01-23 22:11:45 +0000

    Fixed gitsigns auto enabled

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signcolumn = false,
+      numhl = false,
+      linehl = false,
+      word_diff = false,
+      current_line_blame = false,
+    },
+  },
+}
